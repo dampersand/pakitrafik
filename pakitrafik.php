67a37655c198a8ad3f55928ac9c2324838a807c7
dampersand <danputnam1@gmail.com>
2018-07-26 16:17:35 -0700

    header css refresher

---
+++ pakitrafik.php
@@ -0,0 +1,18 @@
+<!DOCTYPE html>
+<html>
+<head>
+	<link rel="stylesheet" href="css/pakitrafik.css">
+</head>
+
+<body>
+	<header>
+		<div class="headerButton">Lorem</div>
+		<div class="headerButton">Ipsum</div>
+		<div class="headerButton">Dolor</div>
+		<div class="headerButton">Sit</div>
+		<div class="headerButton">Amet</div>
+	</header>
+
+</body>
+
+</html>
