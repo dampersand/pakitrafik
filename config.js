46d768334c62feb28ce8c70c4c8e4a1e485b9e96
dampersand <danputnam1@gmail.com>
2018-07-27 18:10:56 -0700

    Going to use node.js... kind of just spitballing stuff here.

--- config.js
+++ config.js
@@ -1,6 +1,7 @@
 /*config.js*/
 
-var config = {}
+var config = {};
+config.redis = {};
 
 config.redis.host="hostname";
 config.redis.port=3000;
--- server.js
+++ server.js
@@ -2,22 +2,37 @@
 
 //constants and instantiations
 const express = require('express');
-const app = express();
+const app 		= express();
 
-var config = ('./config');
+var config 		= require('./config.js');
 
-var test = new Date();
-var testSec = test.getSeconds();
+//precreate all maps in map directory as map objects here.
+//map constructor should load checksum, nodes, links, background, size,
+//anything that might be needed for rendering.
+//we also need a way to scan for any deleted maps, and destroy the object if so.
+//map objects should include a function to re-check checksums.
 
-//Get checksums of initial map files
-app.get('/', (req, res) => {
-	console.log(req);
-	res.send(testSec);
-})
 
-//error logging
-app.use((err, req, res, next) => {
-	res.status(500).send('Will be fleshing out errors here...')
+//handle post requests
+app.post('/', function(req, res, next) {
+	if (req.body.checksum) {res.locals.checksum = req.body.checksum};
+	if (req.body.map) {res.locals.map = req.body.map};
+	next();
+});
+
+//handle get requests.  gets take precedent over posts, because I said so.
+app.get('/', function(req, res, next) {
+	if (req.query.checksum) {res.locals.checksum = req.query.checksum};
+	if (req.query.map) {res.locals.map = req.query.map};
+	next();
+});
+
+//next set of code goes here - determine if map exists, if checksum is correct, etc
+//app.use
+
+//error logging... eventually
+app.use(function(err, req, res, next) {
+	res.sendStatus(500);
 });
 
 app.listen(config.redis.port);
