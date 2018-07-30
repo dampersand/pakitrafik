7ad49dd785905060aa94e26e466d614885469ae9
dampersand <danputnam1@gmail.com>
2018-07-30 15:04:03 -0700

    SAVE before committing, dan...

--- mapworker.js
+++ mapworker.js
@@ -8,41 +8,44 @@ const Promise = require("promise");
 //config files
 var config = require("./config.js");
 
-module.exports = {
-	//returns a promise that should include the checksum for the map in question
-	checkMap: function(mapName, directory = config.package.mapDirectory) {
-		return new Promise(function(resolve, reject) {
-			checksum.file(directory + "/" + mapName, function(err,sum) {
+//functions
+//returns a promise that should include the checksum for the map in question
+function checkMap(mapName, directory = config.package.mapDirectory) {
+	return new Promise(function(resolve, reject) {
+		checksum.file(directory + "/" + mapName, function(err,sum) {
+			if (err) { //TODO: error checking here
+				return reject(err);
+			} else {
+				resolve(sum);
+			}
+		})
+	});
+}
+
+//testing how this returns
+function getMap(mapName, directory = config.package.mapDirectory) {
+	//asynchronously get checksum of map file and read said map from file
+	return new Promise.all([
+		checkMap(mapName, directory), 
+		new Promise(function(resolve,reject) {
+			fs.readFile(directory + "/" + mapName, 'utf8', function(err, data) {
 				if (err) { //TODO: error checking here
 					return reject(err);
 				} else {
-					resolve(sum);
+					resolve(data);
 				}
-			})
-		});
-	},
-	//returns a promise that should include the 
-	getMap: function(mapName, directory = config.package.mapDirectory) {
-		//asynchronously get checksum of map file and read said map from file
-		var fullMapPromise = Promise.all([
-			checkMap(mapName, directory), 
-			new Promise(function(resolve,reject) {
-				fs.readFile(directory + "/" + mapName, 'utf8', function(err, data) {
-					if (err) { //TODO: error checking here
-						return reject(err);
-					} else {
-						resolve(data);
-					}
-				});
-			})
-		]).then(function(result){ //after reading and getting checksum, put into 'map' variable and return 'map' variable
-			var map;
-			map = result[1];
-			map['meta']['checksum'] = result[0]; //TODO: double-check that 'meta' exists
-			return map;
-		});
+			});
+		})
+	]).then(function(result){ //after reading and getting checksum, put into 'map' variable and return 'map' variable
+		var map;
+		map = result[1];
+		map['meta']['checksum'] = result[0]; //TODO: double-check that 'meta' exists
+		return map;
+	});
 
-		return fullMapPromise; //pass the promise along?  not sure if this works the way I want, will need to test.
+}
 
-	}
+module.exports = {
+	checkMap,
+	getMap
 }
