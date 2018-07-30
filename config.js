86003cf1cebbed7da79e5408dd1da4dd474cb589
dampersand <danputnam1@gmail.com>
2018-07-30 14:47:27 -0700

    added performance config parameters.  Started work on mapworker.js, pushing to repo to test on test unit this way cuz I can't get sshfs to work.

--- config.js
+++ config.js
@@ -2,8 +2,24 @@
 
 var config = {};
 config.redis = {};
+config.settings = {};
+config.package = {};
 
+//config.redis
 config.redis.host="hostname";
 config.redis.port=3000;
+
+//config.settings
+//how often (in seconds) to perform checksum testing on the in-memory map set.
+//More often means map structures are updated more often but will increase backend load.
+config.settings.mapSetUpdateInterval = 30;
+
+//whether or not to trust the in-memory map set on fresh map requests
+//true = new requests can be served from the in-memory map set without immediate checksum testing (maps may be out of date until checksum test is performed)
+//false = new requests should always cause a checksum test (resulting in fully up-to-date map, increases backend load)
+config.settings.trustMapSet = false;
+
+//where to find/save maps
+config.package.mapDirectory="./maps";
 
 module.exports = config;
---
+++ mapworker.js
@@ -0,0 +1,48 @@
+/*mapworker.js*/
+
+//required modules
+const fs = require("fs");
+const checksum = require("checksum");
+const Promise = require("promise");
+
+//config files
+var config = require("./config.js");
+
+module.exports = function() {
+	//returns a promise that should include the checksum for the map in question
+	checkMap: function(mapName, directory = config.package.mapDirectory) {
+		return new Promise(function(resolve, reject) {
+			checksum.file(directory + "/" + mapName, function(err,sum) {
+				if (err) { //TODO: error checking here
+					return reject(err);
+				} else {
+					resolve(sum);
+				}
+			}
+		});
+	}
+	//returns a promise that should include the 
+	getMap: function(mapName, directory = config.package.mapDirectory) {
+		//asynchronously get checksum of map file and read said map from file
+		var fullMapPromise = Promise.all([
+			checkMap(mapName, directory), 
+			new Promise(function(resolve,reject) {
+				fs.readFile(directory + "/" + mapName, 'utf8', function(err, data) {
+					if (err) { //TODO: error checking here
+						return reject(err);
+					} else {
+						resolve(data);
+					}
+				});
+			})
+		]).then(function(result){ //after reading and getting checksum, put into 'map' variable and return 'map' variable
+			var map;
+			map = result[1];
+			map['meta']['checksum'] = result[0]; //TODO: double-check that 'meta' exists
+			return map;
+		});
+
+		return fullMapPromise; //pass the promise along?  not sure if this works the way I want, will need to test.
+
+	}
+}
