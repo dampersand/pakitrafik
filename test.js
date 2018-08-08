0e0b19f2af79448adb5561a95b0b88fc31ab4234
dampersand <danputnam1@gmail.com>
2018-08-08 11:12:35 -0700

    forgot a file

---
+++ test.js
@@ -0,0 +1,11 @@
+//test.js
+
+let canvas = document.getElementById('mapCanvas');
+//TODO: experiment with these - canvas scaling
+canvas.width = 600;
+canvas.height = 400;
+canvas.style.height = 400;
+canvas.style.width = 400;
+
+let ctx = canvas.getContext('2d');
+ctx.fillStyle('white');
